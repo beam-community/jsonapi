4f48a07e3cd0c60239f4d1ce5e09065501c277c8
Jason S <jason@rokkincat.com>
2015-06-22 14:09:29 -0500

    Using conn.params if they are not explicitly provided

--- lib/jsonapi/phoenix.ex
+++ lib/jsonapi/phoenix.ex
@@ -6,9 +6,10 @@ defmodule JSONAPI.PhoenixView do
   defmacro __using__(_opts) do
     quote do
       use JSONAPI.View
-      def render("show.json", %{data: data, params: params, conn: conn}), do: show(__MODULE__, data, conn, params)
-      def render("show.json", %{data: data, conn: conn}), do: show(__MODULE__, data, conn, nil)
-      def render("index.json", %{data: data, params: params, conn: conn}), do: index(__MODULE__, data, conn, params)
+      def render("show.json", %{data: data, conn: conn}), do: show(__MODULE__, data, conn, conn.params)
+      def render("show.json", %{data: data, conn: conn, params: params}), do: show(__MODULE__, data, conn, params)
+      def render("index.json", %{data: data, conn: conn}), do: index(__MODULE__, data, conn, conn.params)
+      def render("index.json", %{data: data, conn: conn, params: params}), do: show(__MODULE__, data, conn, params)
     end
   end
 end
