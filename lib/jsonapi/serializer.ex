f9a1f98f511ab92bd6544485cb3c5c6a766e6e94
Jake Robers <jakerobers@gmail.com>
2015-12-22 15:12:53 -0600

    changed to empty?

--- lib/jsonapi/serializer.ex
+++ lib/jsonapi/serializer.ex
@@ -48,7 +48,7 @@ defmodule JSONAPI.Serializer do
     # Handle all the relationships
     Enum.map_reduce(valid_includes, doc, fn({key, rel_view}, acc) ->
       rel_data = Map.get(data, key)   
-      if assoc_loaded?(rel_data) && (!is_nil(rel_data) || !Enum.empty(rel_data)) do #Check if we can handle this
+      if assoc_loaded?(rel_data) && (!is_nil(rel_data) || !Enum.empty?(rel_data)) do #Check if we can handle this
         only_rel_view = get_view(rel_view)
         # Build the relationship url
         rel_url = view.url_for_rel(data, only_rel_view.type(), conn) 
