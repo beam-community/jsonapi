117fe46af09cf46a0a2c6f01d0234054e7b81162
Mitchell Henke <mitchh23@gmail.com>
2015-12-23 13:56:26 -0600

    fix small typo

--- lib/jsonapi/config.ex
+++ lib/jsonapi/config.ex
@@ -1,3 +1,3 @@
 defmodule JSONAPI.Config do
-  defstruct fields: %{}, view: nil, filter: %{}, includes: [], sort: nil, data: nil, required_fileds: nil, opts: nil
+  defstruct fields: %{}, view: nil, filter: %{}, includes: [], sort: nil, data: nil, required_fields: nil, opts: nil
 end
