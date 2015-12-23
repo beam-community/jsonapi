2742e97f8e6a60b7493b3bc0913d7e67b4774b18
Mitchell Henke <mitchh23@gmail.com>
2015-12-23 13:58:43 -0600

    config type

--- lib/jsonapi/config.ex
+++ lib/jsonapi/config.ex
@@ -1,3 +1,14 @@
 defmodule JSONAPI.Config do
+  @type t :: %JSONAPI.Config {
+    fields: map,
+    view: atom,
+    filter: map,
+    includes: list,
+    sort: list,
+    data: any,
+    required_fields: list,
+    opts: list
+  }
+
   defstruct fields: %{}, view: nil, filter: %{}, includes: [], sort: nil, data: nil, required_fields: nil, opts: nil
 end
