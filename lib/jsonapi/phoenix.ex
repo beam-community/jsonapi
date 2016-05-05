a82449b17285c3ff20d234d31e95aa7018d5d0e1
Sonny Scroggin <scrogson@gmail.com>
2016-05-05 01:16:30 -0500

    Pass opts to JSONAPI.View

--- lib/jsonapi/phoenix.ex
+++ lib/jsonapi/phoenix.ex
@@ -3,11 +3,11 @@ defmodule JSONAPI.PhoenixView do
   This is an optional Phoenix specific module to include. It will give you default render show and index.json methods.
   """
 
-  defmacro __using__(_opts) do
+  defmacro __using__(opts \\ []) do
     quote do
-      use JSONAPI.View
       def render("show.json", %{data: data, conn: conn}), do: show(data, conn, conn.params)
       def render("show.json", %{data: data, conn: conn, params: params}), do: show(data, conn, params)
+      use JSONAPI.View, unquote(opts)
 
       def render("index.json", %{data: data, conn: conn}), do: index(data, conn, conn.params)
       def render("index.json", %{data: data, conn: conn, params: params}), do: show(data, conn, params)
