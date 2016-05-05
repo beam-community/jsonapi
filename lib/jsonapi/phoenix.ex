b2c768203327d31f611b07bc3ee88381c1391def
Sonny Scroggin <scrogson@gmail.com>
2016-05-05 01:16:44 -0500

    cleanup

--- lib/jsonapi/phoenix.ex
+++ lib/jsonapi/phoenix.ex
@@ -1,16 +1,23 @@
 defmodule JSONAPI.PhoenixView do
   @moduledoc """
-  This is an optional Phoenix specific module to include. It will give you default render show and index.json methods.
+  This is an optional Phoenix specific module to include.
+
+  It will give you default render clauses for `show.json` and `index.json`.
   """
 
   defmacro __using__(opts \\ []) do
     quote do
-      def render("show.json", %{data: data, conn: conn}), do: show(data, conn, conn.params)
-      def render("show.json", %{data: data, conn: conn, params: params}), do: show(data, conn, params)
       use JSONAPI.View, unquote(opts)
 
-      def render("index.json", %{data: data, conn: conn}), do: index(data, conn, conn.params)
-      def render("index.json", %{data: data, conn: conn, params: params}), do: show(data, conn, params)
+      def render("show.json", %{data: data, conn: conn}),
+        do: show(data, conn, conn.params)
+      def render("show.json", %{data: data, conn: conn, params: params}),
+        do: show(data, conn, params)
+
+      def render("index.json", %{data: data, conn: conn}),
+        do: index(data, conn, conn.params)
+      def render("index.json", %{data: data, conn: conn, params: params}),
+        do: show(data, conn, params)
     end
   end
 end
