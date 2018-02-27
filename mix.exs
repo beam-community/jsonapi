2507fead4bb77ea73708ebe4394eab4b010f99e9
Sean Callan <seancallan@gmail.com>
2018-02-27 10:41:20 -0700

    Update dependencies and version

--- mix.exs
+++ mix.exs
@@ -2,36 +2,35 @@ defmodule JSONAPI.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :jsonapi,
-      version: "0.6.0",
+    [
+      app: :jsonapi,
+      version: "0.7.0",
       package: package(),
       description: description(),
       elixir: "~> 1.0",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/jeregrine/jsonapi",
-      deps: deps()]
-  end
-
-  def application do
-    []
+      deps: deps()
+    ]
   end
 
   defp deps do
     [
       {:plug, "~> 1.0"},
+      {:poison, "~> 3.0"},
       {:ex_doc, "~> 0.7", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:poison, "~> 3.0", only: :test},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp package do
     [
-      maintainers: ["Jason Stiebs", "Mitchell Henke", "Jake Robers"],
+      maintainers: ["Jason Stiebs", "Mitchell Henke", "Jake Robers", "Sean Callan"],
       licenses: ["MIT"],
-      links: %{github: "https://github.com/jeregrine/jsonapi", docs: "http://hexdocs.pm/jsonapi/"}]
+      links: %{github: "https://github.com/jeregrine/jsonapi", docs: "http://hexdocs.pm/jsonapi/"}
+    ]
   end
 
   defp description do
