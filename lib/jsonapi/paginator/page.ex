0918c3873d75cb588ab4d91fa946ff1ad13de4d1
Luca Corti <luca@fantacast.it>
2019-03-25 21:13:13 +0100

    Run formatter

--- lib/jsonapi/paginator/page.ex
+++ lib/jsonapi/paginator/page.ex
@@ -14,17 +14,20 @@ defmodule JSONAPI.Paginator.Page do
       first: first,
       last: last,
       next: next_link(data, view, conn, page),
-      prev: previous_link(data, view, conn, page),
+      prev: previous_link(data, view, conn, page)
     }
   end
 
-  defp next_link(data, view, conn, %{page: page, size: size, total_pages: total_pages}) when page < total_pages,
-    do: view.url_for_pagination(data, conn, %{size: size, page: page + 1})
+  defp next_link(data, view, conn, %{page: page, size: size, total_pages: total_pages})
+       when page < total_pages,
+       do: view.url_for_pagination(data, conn, %{size: size, page: page + 1})
+
   defp next_link(_data, _view, _conn, _page),
     do: nil
 
   defp previous_link(data, view, conn, %{page: page, size: size}) when page > 1,
     do: view.url_for_pagination(data, conn, %{size: size, page: page - 1})
+
   defp previous_link(_data, _view, _conn, _page),
     do: nil
 end
