5d65d20640f9fe61c93f029ce0b1ced9a11535bb
Scott Newcomer <snewcomer24@gmail.com>
2020-05-17 07:28:50 -0700

    missing comma

--- lib/jsonapi/deprecation.ex
+++ lib/jsonapi/deprecation.ex
@@ -10,7 +10,7 @@ defmodule JSONAPI.Deprecation do
   def warn(:query_parser_fields) do
     IO.warn(
       "`JSONAPI.QueryParser` will no longer accept `fields` query params that refer to the relationship key of a `JSONAPI.View`.  Please use the `type` of the resource to perform filtering.
-      See: https://github.com/jeregrine/jsonapi/pull/203."
+      See: https://github.com/jeregrine/jsonapi/pull/203.",
       Macro.Env.stacktrace(__ENV__)
     )
   end
