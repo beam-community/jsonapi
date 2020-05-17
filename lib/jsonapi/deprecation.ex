ff24050f914e85b6540d0c34ac67ef372263def2
Scott Newcomer <snewcomer24@gmail.com>
2020-05-17 07:10:17 -0700

    1.6?

--- lib/jsonapi/deprecation.ex
+++ lib/jsonapi/deprecation.ex
@@ -10,8 +10,7 @@ defmodule JSONAPI.Deprecation do
   def warn(:query_parser_fields) do
     IO.warn(
       "`JSONAPI.QueryParser` will no longer accept `fields` query params that refer to the relationship key of a `JSONAPI.View`.  Please use the `type` of the resource to perform filtering.
-      See: https://github.com/jeregrine/jsonapi/pull/203.",
-      Macro.Env.stacktrace(__ENV__)
+      See: https://github.com/jeregrine/jsonapi/pull/203."
     )
   end
 end
