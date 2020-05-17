1668fa5d7606d7590a5d87fcb307660aaaffb5a6
Scott Newcomer <snewcomer24@gmail.com>
2020-05-17 07:23:04 -0700

    is 1.6 tripping the rest?

--- lib/jsonapi/deprecation.ex
+++ lib/jsonapi/deprecation.ex
@@ -11,6 +11,7 @@ defmodule JSONAPI.Deprecation do
     IO.warn(
       "`JSONAPI.QueryParser` will no longer accept `fields` query params that refer to the relationship key of a `JSONAPI.View`.  Please use the `type` of the resource to perform filtering.
       See: https://github.com/jeregrine/jsonapi/pull/203."
+      Macro.Env.stacktrace(__ENV__)
     )
   end
 end
