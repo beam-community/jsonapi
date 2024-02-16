40568d482dab1bd713e5cdc668ccfc70bd36cc5b
Mathew Polzin <matt.polzin@gmail.com>
2024-02-15 19:21:16 -0600

    bump version to release new field transformation options

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JSONAPI.Mixfile do
   def project do
     [
       app: :jsonapi,
-      version: "1.6.3",
+      version: "1.7.0",
       package: package(),
       compilers: compilers(Mix.env()),
       description: description(),
